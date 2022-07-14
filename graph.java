8cfa6549afd0a7de061cdbc91101d73f4908d5bf
deepakkdkk <deepakkumardk2811@gmail.com>
2022-07-15 00:19:12 +0530

    22 june done

--- graph.java
+++ graph.java
@@ -620,6 +620,302 @@ class Solution
             }
         }
     }
+}
+
+// LC: 133 Clone Graph
+class Solution {
+    public Node cloneGraph(Node node) {
+        if(node == null){
+            return null;
+        }
+        HashMap<Integer, Node> visited = new HashMap<>();
+        return helper(node, visited);
+    }
     
+    public Node helper(Node node, HashMap<Integer, Node> visited){
+        Node nodeClone = new Node(node.val);
+        visited.put(node.val, nodeClone);
+        
+        for(Node nbr : node.neighbors){
+            if(!visited.containsKey(nbr.val)){
+                Node nbrClone = helper(nbr, visited);
+                nodeClone.neighbors.add(nbrClone);
+            }else{
+                Node nbrClone = visited.get(nbr.val);
+                nodeClone.neighbors.add(nbrClone);
+            }
+        }
+        return nodeClone;        
+    }
+}
+
+// GFG Alien Dictionary
+class Solution
+{
+    public String findOrder(String [] dict, int N, int K)
+    {
+        // Write your code here
+        ArrayList<Integer>[] graph = (ArrayList<Integer>[])new ArrayList[K];
+        for(int i = 0; i < K; i++){
+            graph[i] = new ArrayList<>();
+        }
+        
+        for(int i = 0; i < dict.length - 1; i++){
+            String word1 = dict[i];
+            String word2 = dict[i + 1];
+            
+            for(int j = 0; j < Math.min(word1.length(), word2.length()); j++){
+                char ch1 = word1.charAt(j);
+                char ch2 = word2.charAt(j);
+                
+                if(ch1 != ch2){
+                    graph[ch1 - 'a'].add(ch2 - 'a');
+                    break;
+                }
+            }
+        }
+        
+        boolean[] vis = new boolean[K];
+        Stack<Integer> st = new Stack<>();
+        
+        
+        for(int i = 0; i < K; i++){
+            if(!vis[i]){
+                tsort(graph, vis, st, i);
+            }
+        }
+     
+        String ans = "";
+        while(st.size() > 0){
+            ans += (char)(st.pop() + 'a');
+        }
+        
+        // System.out.println(ans);
+        
+        return ans;
+    }
     
+    public void tsort(ArrayList<Integer>[] graph, boolean[] vis, Stack<Integer> st, int v){
+        vis[v] = true;
+        for(int n: graph[v]){
+            if(!vis[n]){
+                tsort(graph, vis, st, n);
+            }
+        }
+        
+        st.push(v);
+    }
+}
+
+// GFG Topological based indegree based solution (Kanh's Algorithm)
+class Solution
+{
+    //Function to return list containing vertices in Topological order. 
+    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
+    {
+        // add your code here
+        int[] tsort = new int[V];
+        
+        int[] inDegree = new int[V];
+        for(int v = 0; v < V; v++){
+            for(int n: adj.get(v)){
+                inDegree[n]++;
+            }
+        }
+        
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        for(int v = 0; v < V; v++){
+            if(inDegree[v] == 0){
+                queue.add(v);
+            }
+        }
+        
+        int idx = 0;
+        while(queue.size() > 0){
+            int v = queue.remove();
+            tsort[idx++] = v;
+            
+            for(int n: adj.get(v)){
+                inDegree[n]--;
+                if(inDegree[n] == 0){
+                    queue.add(n);
+                }
+            }
+        }
+        
+        return tsort;
+    }
+}
+
+// LC : 210 Course Schedule II
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] ans = new int[numCourses];
+        
+        int[] in = new int[numCourses];
+        for(int[] edge: prerequisites){
+            in[edge[1]]++;
+        }
+        
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        for(int course = 0; course < numCourses; course++){
+            if(in[course] == 0){
+                queue.add(course);
+            }
+        }
+        
+        int idx = ans.length - 1;
+        while(queue.size() > 0){
+            int course = queue.remove();
+            ans[idx--] = course;
+            
+            for(int[] edge: prerequisites){
+                if(edge[0] == course){
+                    in[edge[1]]--;
+                    
+                    if(in[edge[1]] == 0){
+                        queue.add(edge[1]);
+                    }
+                }
+            }
+        }
+        
+        if(idx != -1){
+            return new int[]{};
+        }
+        
+        return ans;
+    }
+}
+
+// Kruskal's Algorithm
+public static void kruskals(ArrayList<Edge>[] graph){
+    PriorityQueue<Edge> pq = new PriorityQueue<>();
+    for(int v = 0; v < graph.length; v++){
+        for(Edge e: graph[v]){
+            pq.add(e);
+        }
+    }
+    
+    parent = new int[graph.length];
+    rank = new int[graph.length];
+    for(int i = 0; i < graph.length; i++){
+        parent[i] = i;
+        rank[i] = 0;
+    }
+    
+    while(pq.size() > 0){
+        Edge e = pq.remove();
+        
+        int srcLead = find(e.src);
+        int nbrLead = find(e.nbr);
+        
+        if(srcLead != nbrLead){
+            System.out.println(e.src + "-" + e.nbr + "@" + e.wt);
+            
+            union(srcLead, nbrLead);
+        }
+    }
+}
+
+static int[] parent;
+static int[] rank;
+
+public static int find(int x){
+    if(parent[x] == x){
+        return x;
+    } else {
+        parent[x] = find(parent[x]);
+        return parent[x];
+    }
+}
+
+public static void union(int s1l, int s2l){
+    if(rank[s1l] < rank[s2l]){
+        parent[s1l] = s2l;
+    } else if(rank[s2l] < rank[s1l]){
+        parent[s2l] = s1l;
+    } else {
+        parent[s1l] = s2l;
+        rank[s2l]++;
+    }
+}
+
+// LC : 305 No. of Islands 2
+class Solution {
+    
+    public  List<Integer> numIslands2(int m, int n, int[][] positions) {
+    int[][] grid = new int[m][n];
+    parent = new int[m * n];
+    rank = new int[m * n];
+    for(int i = 0; i < m * n; i++){
+        parent[i] = i;
+        rank[i] = 0;
+    }
+    ArrayList<Integer> res = new ArrayList<>();
+    count = 0;
+    
+    for(int[] pos: positions){
+        int x = pos[0];
+        int y = pos[1];
+        
+        if(grid[x][y] == 0){    // Here hashset would not work for checking contains function after making pair class 
+            count++;
+        }
+        
+        
+        grid[x][y] = 1;
+        
+        handleNewCell(x, y, x - 1, y, m, n, grid);
+        handleNewCell(x, y, x + 1, y, m, n, grid);
+        handleNewCell(x, y, x, y - 1, m, n, grid);
+        handleNewCell(x, y, x, y + 1, m, n, grid);
+        
+        res.add(count);
+    }
+    
+    return res;
+  }
+  
+  public void handleNewCell(int x, int y, int xx, int yy, int m, int n, int[][] grid){
+      if(xx < 0 || yy < 0 || xx >= m || yy >= n || grid[xx][yy] == 0){
+          return;
+      }
+      
+      int xyCell = x * n + y;
+      int xxyyCell = xx * n + yy;
+      
+      int xylead = find(xyCell);
+      int xxyylead = find(xxyyCell);
+      
+      if(xylead != xxyylead){
+          count--;
+          union(xylead, xxyylead);
+      }
+  }
+  
+   int count;
+   int[] parent;
+   int[] rank;
+  
+  
+  public int find(int i){
+      if(parent[i] == i){
+          return i;
+      } else {
+          parent[i] = find(parent[i]);
+          return parent[i];
+      }
+  }
+  
+  public void union(int i, int j){
+      if(rank[i] < rank[j]){
+          parent[i] = j;
+      } else if(rank[j] < rank[i]){
+          parent[j] = i;
+      } else {
+          parent[i] = j;
+          rank[j]++;
+      }
+  }
 }
