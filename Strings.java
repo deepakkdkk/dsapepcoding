00d7ed0e8d8f9a16a58ca31d46c47f15f4525f66
deepakkdkk <deepakkumardk2811@gmail.com>
2024-04-07 14:52:03 +0530

    z function added

--- Strings.java
+++ Strings.java
@@ -2,8 +2,8 @@
 
 public class Strings{
 
-    //for lps
-    public void lps(String s){
+    //for lps (proper prefix ending at index i) (pattern + # + text)
+    public int[] lps(String s){
         int[] lps = new int[s.length()];
         int i = 1;
         int len = 0;
@@ -22,7 +22,25 @@ public class Strings{
             }
             
         }
+        return lps;
     }
 
-    
+    //Z function (proper prefix starting at index i)  (pattern + # + text)
+    public int[] zfunc(String s){
+        int z[] = new int[s.length()];
+        int l = 0, r = 0;
+        for(int i = 1; i < s.length(); i++){
+            if(i <= r){
+                z[i] = Math.min(r - i + 1, z[i - l]);
+            }
+            while(i + z[i] < s.length() && (s.charAt(z[i]) == s.charAt(i + z[i]))){
+                z[i]++;
+            }
+            if(i + z[i] - 1 > r){
+                l = i;
+                r = i + z[i] - 1;
+            }
+        }
+        return z;
+    }
 }
